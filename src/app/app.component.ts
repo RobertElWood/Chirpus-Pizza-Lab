23912b08f2ec30b2e9975ae69b0cf790ef9552c0
Robert Woods <robert.woods@fulbrightmail.org>
2022-11-10 08:23:00 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/MenuItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChirpusPizza';
+  title = 'ChirpusPizza Menu';
+
+  m1 : MenuItem = {id: 1, name: 'Cheesy Bread', price: 4.99};
+  m2 : MenuItem = {id: 2, name: 'Cheese Pizza', price: 11.99};
+  m3 : MenuItem = {id: 3, name: 'Calzone', price: 9.99};
+  m4 : MenuItem = {id: 4, name: 'Pepperoni Pizza', price: 12.50};
+  m5 : MenuItem = {id: 5, name: 'Supreme Pizza', price: 13.00};
+  m6 : MenuItem = {id: 5, name: 'Pizza Size Upgrade:', price: 2.50};
+
+  menu : MenuItem[] = [this.m1, this.m2, this.m3, this.m4, this.m5, this.m6]
 }
+
